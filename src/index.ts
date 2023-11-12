868ae9b90ffbffa5210795dcc00ae569e917db54
Terence Tachiona <tachiona0607@gmail.com>
2023-11-12 11:17:05 +0200

    create local env

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = ''; // DB URI
+const MONGO_URL = 'mongodb://localhost:27017 node index.js'; // DB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
