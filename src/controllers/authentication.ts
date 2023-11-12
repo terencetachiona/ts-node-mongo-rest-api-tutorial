0133e7c5a81bf6fb31c96cb44c45185fca17dc92
Terence Tachiona <tachiona0607@gmail.com>
2023-11-12 12:29:01 +0200

    Update authentication.ts

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import { getUserByEmail, createUser } from '../db/users';
 import { authentication, random } from '../helpers';
 
+
+
 export const login = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password } = req.body;
@@ -28,7 +30,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
     await user.save();
 
-    res.cookie('ANTONIO-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+    res.cookie('SHONA-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
 
     return res.status(200).json(user).end();
   } catch (error) {
